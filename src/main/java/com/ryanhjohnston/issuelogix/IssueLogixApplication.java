8ab04010ef9549d5cf61fbae455e213517957a44
ryanhjohnston <ryanhjohnstoncollege@gmail.com>
2023-07-06 17:24:33 -0500

    Empty project

---
+++ src/main/java/com/ryanhjohnston/issuelogix/IssueLogixApplication.java
@@ -0,0 +1,13 @@
+package com.ryanhjohnston.issuelogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueLogixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueLogixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanhjohnston/issuelogix/IssueLogixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanhjohnston.issuelogix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueLogixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
